4f6dd0d516f99762d16f263af184956927a25ed7
Matthew Cain <codingdatum@gmail.com>
2022-03-03 13:04:01 -0500

    gh pages for statis deployment

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -65,15 +65,11 @@ const CartProvider = props => {
     const [cartState, dispatchCartAction ] = useReducer(cartReducer, defaultCartState);
 
     const addItemToCartHandler = item => {
-
         dispatchCartAction({type: "ADD", item: item})
-
     }
 
     const removeItemFromCartHandler = id => {
-
         dispatchCartAction({type: "REMOVE", id: id})
-
     }
 
     const cartContext = {
